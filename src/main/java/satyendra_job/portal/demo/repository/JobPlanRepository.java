123184e2dd111ac63378f7ba5949b960b67aeb1c
Snehapal2005 <snehapal9584@gmail.com>
2026-04-01 17:35:49 +0530

    Delete src directory

--- src/main/java/satyendra_job/portal/demo/repository/JobPlanRepository.java
+++
@@ -1,7 +0,0 @@
-package satyendra_job.portal.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import satyendra_job.portal.demo.model.JobPlan;
-
-public interface JobPlanRepository extends JpaRepository<JobPlan, Long> {
-}
